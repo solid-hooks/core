1dc454b8c7b63a928d3ab5ded53854d530ea0b88
subframe7536 <1667077010@qq.com>
2024-04-06 19:15:04 +0800

    chore!: remove tick

--- src/tick.ts
+++
@@ -1,7 +0,0 @@
-const _promise = Promise.resolve()
-/**
- * Vue-like next tick
- */
-export function useTick<T = void>(this: T, fn?: (this: T) => void): Promise<void> {
-  return _promise.then(this ? fn?.bind(this) : fn)
-}
