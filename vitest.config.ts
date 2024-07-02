496625e37fd0a060a8dfe24eabb730483ce1847c
subframe7536 <1667077010@qq.com>
2024-07-02 19:18:36 +0800

    chore: release v0.3.5

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
+  // @ts-expect-error ignore
   plugins: [solid()],
   test: {
     environment: 'jsdom',
