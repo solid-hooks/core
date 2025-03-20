c5e6adabea0fef50586dc78831a19b3c75f594aa
subframe7536 <1667077010@qq.com>
2025-03-20 13:13:03 +0800

    test: add `useProps` in playground

--- dev/components/context-provider.tsx
+++ dev/components/context-provider.tsx
@@ -1,6 +1,6 @@
 import { catchError } from 'solid-js'
 
-import { createContextProvider, createRef } from '../../src'
+import { createContextProvider, createRef, useProps } from '../../src'
 
 export const [TestProvider, useTestContext] = createContextProvider((param: { initial: number }) => {
   const count = createRef(param.initial)
@@ -12,24 +12,30 @@ export const [TestProvider, useTestContext] = createContextProvider((param: { in
   }
 })
 
-function Child() {
+function Child(props: { data: number }) {
+  const [pick] = useProps(props, ['data'])
   const { count, increment } = useTestContext()
   return (
     <>
       <button class="btn" onClick={increment}>
-        {count()}
+        context: {count()}
       </button>
+      <div>
+        props: {pick().data + ''}
+      </div>
     </>
   )
 }
 
 export function TestContextProvider() {
+  const data = createRef(1)
   catchError(() => {
     console.log('[useContextProvider] call useTestContext() outside provider:', useTestContext())
   }, () => { })
   return (
     <TestProvider initial={0}>
-      <Child />
+      <button onClick={() => data(val => val * 2)}>click to test props</button>
+      <Child data={data()} />
     </TestProvider>
   )
 }
