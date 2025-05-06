8e705275eb510869b4ebbab448ca44400c76c0f6
subframe7536 <1667077010@qq.com>
2025-05-06 10:24:19 +0800

    feat: override clipboardItem

--- src/web/clipboard/signal.ts
+++ src/web/clipboard/signal.ts
@@ -4,11 +4,18 @@ import { createSignal } from 'solid-js'
 
 export type ClipboardItemSignal<T> = [accessor: Accessor<ClipboardItem>, set: (data: T) => ClipboardItem]
 
-export function generateClipboardItem(part: BlobPart, mime: string): ClipboardItem {
-  return new ClipboardItem({ [mime]: new Blob([part], { type: mime }) })
+export function generateClipboardItem(blob: Blob): ClipboardItem
+export function generateClipboardItem(part: BlobPart, mime: string): ClipboardItem
+export function generateClipboardItem(data: Blob | BlobPart, mime?: string): ClipboardItem {
+  if (mime) {
+    return new ClipboardItem({ [mime]: new Blob([data], { type: mime }) })
+  }
+  return new ClipboardItem({ [(data as Blob).type]: data as Blob })
 }
 
-export function createClipboardItem<T extends BlobPart>(part: T, mime: string): ClipboardItemSignal<T> {
-  const [item, setItem] = createSignal(generateClipboardItem(part, mime))
-  return [item, data => setItem(generateClipboardItem(data, mime))]
+export function createClipboardItem(blob: Blob): ClipboardItemSignal<Blob>
+export function createClipboardItem<T extends BlobPart>(part: T, mime: string): ClipboardItemSignal<T>
+export function createClipboardItem<T extends BlobPart>(data: T, mime?: string): ClipboardItemSignal<T> | ClipboardItemSignal<Blob> {
+  const [item, setItem] = createSignal(generateClipboardItem(data, mime as any))
+  return [item, (data: any) => setItem(generateClipboardItem(data, mime as any))]
 }
